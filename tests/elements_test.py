f49f642ecf1199fbf366212a4385a9851e9a682d
dmadiev <dmadiev@sipuni.ru>
2025-06-03 14:11:45 +0500

    update elements tests added tests:

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,7 @@
+import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, WebTablePage
 from conftest import driver
 
 
@@ -28,4 +29,39 @@ class TestElements:
             print(input_checkbox)
             print(output_result)
             assert input_checkbox == output_result, 'checkboxes have not been selected'
-            
+
+    class TestRadioButton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "'Yes' have not been selected"
+            assert output_impressive == 'Impressive', "'Impressive' have not been selected"
+            assert output_no == 'No', "'No' have not been selected"
+
+    class TestWebTable:
+
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            print(new_person)
+            print(table_result)
+            assert new_person in table_result
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0,5)]
+            web_table_page.search_some_person(key_word)
+            table_result = web_table_page.check_search_person()
+            print(key_word)
+            print(table_result)
+            assert key_word in table_result , "the person was not found in the table"
