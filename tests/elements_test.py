cd37c20c34cd9202758c846e6b7f0d9b86db780d
dmadiev <dmadiev@sipuni.ru>
2025-06-17 12:59:42 +0500

    update elements tests
    added class:
      TestUploadAndDownload
    added tests:
    - test_upload_file
    - test_download_file

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,9 @@
+import os
 import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, WebTablePage, RadioButtonPage, ButtonsPage, LinksPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, WebTablePage, RadioButtonPage, ButtonsPage, LinksPage, \
+    UploadAndDownloadPage
 from conftest import driver
 
 
@@ -115,3 +117,17 @@ class TestElements:
             response_code = links_page.check_broken_link('https://demoqa.com/bad-request')
             assert response_code == 400, "the link works or the status code is not 400 "
 
+    class TestUploadAndDownloadPage:
+
+        def test_upload_file(self, driver):
+            upload_download_page = UploadAndDownloadPage(driver, 'https://demoqa.com/upload-download')
+            upload_download_page.open()
+            file_name, result = upload_download_page.upload_file()
+            assert file_name == result, "The file has not been uploaded"
+
+
+        def test_download_file(self, driver):
+            upload_download_page = UploadAndDownloadPage(driver, 'https://demoqa.com/upload-download')
+            upload_download_page.open()
+            check = upload_download_page.download_file()
+            assert check is True, "The file has not been downloaded"
