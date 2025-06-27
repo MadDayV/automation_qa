32eeef276989f90f5f1a35765b3c72405697652a
dmadiev <dmadiev@sipuni.ru>
2025-06-27 12:14:52 +0500

    update elements tests
    added class:
      TestDynamicPropertiesPage
    added tests:
    - test_dynamic_properties
    - test_appear_button
    - test_enable_button

---
+++ tests/alerts_frame_windows_test.py
@@ -0,0 +1,21 @@
+import time
+
+from pages.alerts_frame_windows_page import BrowserWindowsPage
+from conftest import driver
+
+
+class TestAlertsFrameWindow:
+
+    class TestBrowserWindows:
+
+        def test_new_tab(self, driver):
+            browser_windows_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+            browser_windows_page.open()
+            text_result = browser_windows_page.check_opened_new_tab()
+            assert text_result == 'This is a sample page', "the new tab has not opened or incorrect tab has opened"
+
+        def test_new_windows(self, driver):
+            browser_windows_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+            browser_windows_page.open()
+            text_result = browser_windows_page.check_opened_new_tab()
+            assert text_result == 'This is a sample page', "the window has not opened or incorrect window has opened"
