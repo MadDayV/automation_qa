83614366be60e9f5e51a90d8ee859dc39e3aaded
dmadiev <dmadiev@sipuni.ru>
2025-06-30 17:57:40 +0500

    created alerts tests
    added class:
      TestAlertsPage
    added tests:
    - test_see_alert
    - test_alert_appear_5_sec
    - test_confirm_alert
    - test_prompt_alert

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 from conftest import driver
 
 
@@ -19,3 +19,33 @@ class TestAlertsFrameWindow:
             browser_windows_page.open()
             text_result = browser_windows_page.check_opened_new_tab()
             assert text_result == 'This is a sample page', "the window has not opened or incorrect window has been opened"
+
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', "Alert didn't show up"
+
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', "Alert didn't show up"
+
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', "Alert didn't show up"
+
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, "Alert didn't show up"
