49dec25933735e7de63092589d12e2a1e6270182
dmadiev <dmadiev@sipuni.ru>
2025-07-01 14:28:21 +0500

    added class:   TestFramesPage added tests: - test_frames

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage
 from conftest import driver
 
 
@@ -49,3 +49,13 @@ class TestAlertsFrameWindow:
             alert_page.open()
             text, alert_text = alert_page.check_prompt_alert()
             assert text in alert_text, "Alert didn't show up"
+
+    class TestFramesPage:
+
+        def test_frames(self, driver):
+            frame_page = FramesPage(driver, 'https://demoqa.com/frames')
+            frame_page.open()
+            result_frame1 = frame_page.check_frame('frame1')
+            result_frame2 = frame_page.check_frame('frame2')
+            assert result_frame1 == ['This is a sample page', '500px', '350px'], 'The frame does not exist'
+            assert result_frame2 == ['This is a sample page', '100px', '100px'], 'The frame does not exist'
