f7985dab276ee0028bce3b9c7f464472d88b9ce0
dmadiev <dmadiev@sipuni.ru>
2025-08-11 19:31:19 +0500

    created class interactions created test Sortable

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -11,5 +11,5 @@ class TestInteractions:
             sortable_page.open()
             list_before, list_after = sortable_page.change_list_order()
             grid_before, grid_after = sortable_page.change_grid_order()
-            assert list_before != list_after, 'the order of the list has not been changed'
-            assert grid_before != grid_after, 'the order of the grid has not been changed'
+            assert list_before != list_after, 'The order of the list has not been changed'
+            assert grid_before != grid_after, 'The order of the grid has not been changed'
